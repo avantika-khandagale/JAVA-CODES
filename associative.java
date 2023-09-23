c1401bf39bcee91d285e29a7e8fd2df5973ab2a6
avantika-khandagale <144552942+avantika-khandagale@users.noreply.github.com>
2023-09-23 15:07:14 +0530

    associative.java
    write the following expression in java program

---
+++ associative.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]){
+        float v=4;
+        float u=2;
+        float a=2;
+        float s=8;
+        float exp=(v*v-u*u)/(2*a*s);
+        System.out.println(exp);
+    }
+}
